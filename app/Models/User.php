806d82bd4508b62c8de0e19eb4ee737068930f1b
Saroj Shrestha <saroj.s@shikhartech.com>
2024-04-17 15:07:34 +0545

    authentication added

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -20,7 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'status'
     ];
+
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -43,5 +47,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
+use App\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(MainController::class)->group(function () {
-    Route::get('/pf/products/{productId}', 'getPrintFulProductById');
-    Route::post('/pf/orders/create', 'printFulCreateOrder');
+    Route::post('/users/register', [AuthController::class, 'register']);
+    Route::post('/users/login', [AuthController::class, 'login']);
+
+Route::middleware([Authenticate::class])->group(function () {
+    Route::controller(MainController::class)->group(function () {
+        Route::get('/pf/products/{productId}', 'getPrintFulProductById');
+        Route::post('/pf/orders/create', 'printFulCreateOrder');
+    });
 });
